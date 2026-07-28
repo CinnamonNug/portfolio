df498f9ee152b5ad5bc1b6cfd3e7f1d4ae64087f
CinnamonNug <cengizcimen007@gmail.com>
2026-07-29 01:39:59 +1000

    added github deploy flow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { solidStart } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    preset: "static", // Tells Nitro to output static HTML/JS files for GitHub Pages
+    baseURL: "/portfolio/", // Matches your repository name
+  },
+  
   plugins: [
     solidStart(),
     tailwindcss(),
