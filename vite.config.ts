b525f34e66528b5b598885d08aaa09f5b7e6cb3b
CinnamonNug <cengizcimen007@gmail.com>
2026-07-29 12:16:53 +1000

    changed workflow files

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,14 @@ import { nitro } from "nitro/vite";
 import { solidStart } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  server: {
-    preset: "static", // Tells Nitro to output static HTML/JS files for GitHub Pages
-    baseURL: "/portfolio/", // Matches your repository name
-  },
-  
+export default defineConfig({  
   plugins: [
     solidStart(),
     tailwindcss(),
-    nitro()
+    nitro(
+      {
+      preset: "static",
+      baseURL: "/portfolio/" // Ensure 'portfolio' matches your GitHub repository name exactly
+    })
   ]
 });
