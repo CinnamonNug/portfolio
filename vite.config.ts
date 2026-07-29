87a6fabaef7079970caada21e9d0f6f63ea1b5af
CinnamonNug <cengizcimen007@gmail.com>
2026-07-29 13:46:48 +1000

    fix: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,11 @@
 import { defineConfig } from "vite";
 import { solidStart } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  // 1. Vite's native property handles the asset sub-directory routing prefix
   base: "/portfolio/",
-
   plugins: [
-    nitro({
-        preset: "static", // Forces the production SSG crawl
-        prerender: {
-          routes: ["/"],
-          crawlLinks: true // Restores your original crawler link discovery
-        }
-      }),
     solidStart(),
-    tailwindcss()
-  ]
+    tailwindcss(),
+  ],
 });
